e8e44c81d039923c6645ce48fb4d16d26db942e5
Carlos Scheidegger <cscheid@cscheid.net>
2021-05-24 12:10:14 -0700

    more js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
+function setupReferences(sel)
+{
+    sel.text(d => d.citationKey);
+}
+
 window.addEventListener('DOMContentLoaded', (event) => {
     let refsEl = d3.selectAll("#refs").node();
     let bibtexEntries = bibtexParse.toJSON(refs.innerText);
     let header = refsEl.previousElementSibling;
 
+    console.log(bibtexEntries);
+    
+    d3.select("#refs").remove();
     d3.select("#refslist").append("ul")
         .selectAll("li")
         .data(bibtexEntries)
         .enter()
         .append("li")
-        .text(d => d.citationKey);
+        .call(setupReferences);
 });
