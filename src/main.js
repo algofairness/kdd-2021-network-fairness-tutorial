2ad9609f5c72b52d72cc44f3f8da31b94f035f4e
Carlos Scheidegger <cscheid@cscheid.net>
2021-05-24 12:20:56 -0700

    more js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 function setupReferences(sel)
 {
-    sel.text(d => d.citationKey);
+    let articles = sel.filter(d => d.entryType === 'article');
+    articles.append("span").text(d => `${d.entryTags.author}. `);
+    articles.append("span").append("em").text(d => `${d.entryTags.title}. `);
+    articles.append("span").text(d => `${d.entryTags.journal}. `);
+    articles.filter(d => d.entryTags.volume !== undefined).append("span").text(d => d.entryTags.volume);
+    articles.filter(d => d.entryTags.number !== undefined).append("span").text(d => `:${d.entryTags.number}`);
+    articles.filter(d => d.entryTags.pages !== undefined).append("span").text(d => ` (${d.entryTags.pages})`);
+    articles.filter(d => (d.entryTags.volume !== undefined ||
+                          d.entryTags.number !== undefined ||
+                          d.entryTags.pages !== undefined)). append("span").text(". ");
+    articles.filter(d => d.entryTags.publisher !== undefined).append("span").text(d => `${d.entryTags.pages}, `);
+    articles.append("span").text(d => `${d.entryTags.year}.`);
+
+    let confprocs = self.filter(d => d.entryType === 'inproceedings');
+
+    confprocs.append("span").text(d => `${d.entryTags.author}. `);
+    confprocs.append("span").append("em").text(d => `${d.entryTags.title}. `);
+    confprocs.append("span").text(d => `${d.entryTags.booktitle}. `);
+    confprocs.filter(d => d.entryTags.pages !== undefined).append("span").text(d => ` ${d.entryTags.pages}.`);
+    confprocs.append("span").text(d => `${d.entryTags.year}.`);
+    
+    // sel.text(d => d.citationKey);
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
